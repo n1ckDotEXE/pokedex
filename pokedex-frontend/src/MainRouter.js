80c93b120143f6a1a51326d1c8b67f0e63f6e6a0
n1ckDotEXE <admin@nicholasnam.com>
2021-04-06 11:42:53 -0400

    Adds Pokemon friends backend

--- pokedex-frontend/src/MainRouter.js
+++ pokedex-frontend/src/MainRouter.js
@@ -5,7 +5,7 @@ import Navbar from "./component/Navbar/Navbar";
 import Home from "./component/Home/Home";
 import PrivateRoute from "./component/PrivateRoute/PrivateRoute";
 import Login from "./component/Login/Login";
-import MyPokemon from "./component/MyPokemon/MyPokemon";
+import CreateFriend from "./component/CreateFriend/CreateFriend";
 import SignUp from "./component/SignUp/SignUp";
 import NotFound from "./component/NotFound/NotFound";
 import AuthHome from "./component/Home/AuthHome";
@@ -18,13 +18,17 @@ const MainRouter = (props) => {
 				handleUserLogout={props.handleUserLogout}
 			/>
 			<Switch>
-				<PrivateRoute exact path="/my-pokemon" component={MyPokemon} />
+				<PrivateRoute
+					exact
+					path="/my-pokemon"
+					component={CreateFriend}
+				/>
 				<Route exact path="/login" component={Login} />
 				<Route exact path="/sign-up" component={SignUp} />
 				<Route exact path="/home" component={Home} />
 				<Route exact path="/auth-home" component={AuthHome} />
 				<Route exact path="/" component={Home} />
-				<Route exact path="/my-pokemon" component={MyPokemon} />
+				{/* <Route exact path="/my-pokemon" component={CreateFriend} /> */}
 				<Route path="*" component={NotFound} />
 			</Switch>
 		</Router>
